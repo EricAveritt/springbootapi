8b54af6862adb43f6ed7ede0d661511a020ae435
EricAveritt <erictheguyjin@gmail.com>
2023-06-27 14:38:24 -0500

    Added tests

--- src/main/java/com/ericaveritt/springbootapi/GreetingController.java
+++ src/main/java/com/ericaveritt/springbootapi/GreetingController.java
@@ -2,6 +2,7 @@ package com.ericaveritt.springbootapi;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
